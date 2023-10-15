5201f52f877997a0d3bb5c19216401b43b670b9a
saisuryavinay <147791886+saisuryavinay@users.noreply.github.com>
2023-10-15 13:30:55 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     if(a>21)     {         printf("HOT");     }     else if(a=20)     {         printf("COLD");     }     else     {         printf("COLD");     } }

---
+++ Climate.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>21)
+    {
+        printf("HOT");
+    }
+    else if(a=20)
+    {
+        printf("COLD");
+    }
+    else
+    {
+        printf("COLD");
+    }
+}
