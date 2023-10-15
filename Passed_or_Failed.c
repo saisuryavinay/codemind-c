684bf607b1111910b632774f1c610314b3e2c477
saisuryavinay <147791886+saisuryavinay@users.noreply.github.com>
2023-10-15 13:18:57 +0530

    #include<stdio.h> int main() {     int e,m,p,c,cs;     scanf("%d%d%d%d%d",&e,&m,&p,&c,&cs);     if((e>35) and (m>35) and (p>35) and (c>35) and (cs>35))     {         printf("PASSED");     }     else     {         printf("FAILED");     } }

---
+++ Passed_or_Failed.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int e,m,p,c,cs;
+    scanf("%d%d%d%d%d",&e,&m,&p,&c,&cs);
+    if((e>35) and (m>35) and (p>35) and (c>35) and (cs>35))
+    {
+        printf("PASSED");
+    }
+    else
+    {
+        printf("FAILED");
+    }
+}
